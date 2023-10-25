bc59e42848addf9be8e1c9dcff80ff62ad796eef
Tree-Person <cristhianwmontiel@gmail.com>
2023-10-24 21:04:00 -0700

    spotify

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+import {useState , useEffect} from "react"
+import AuthModal from "@/Components/AuthModal";
+import UploadModal from "@/Components/UploadModal";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
+    if(!isMounted) {
+        return null
+    }
+
+
+
+    return (
+        <>
+            <AuthModal />
+            <UploadModal />
+        </>
+    )
+}
+
+export default ModalProvider

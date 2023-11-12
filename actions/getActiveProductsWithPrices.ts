98df902365af5a737139676efb6118193ef42f19
Tree-Person <cristhianwmontiel@gmail.com>
2023-11-11 18:45:05 -0800

    spotify

--- actions/getActiveProductsWithPrices.ts
+++ actions/getActiveProductsWithPrices.ts
@@ -1,26 +1,26 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+// import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+// import { cookies } from "next/headers";
 
-import { ProductWithPrice } from "@/types";
+// import { ProductWithPrice } from "@/types";
 
-const getActiveProductsWithPrices = async (): Promise<ProductWithPrice[]> => {
-  const supabase = createServerComponentClient({
-    cookies: cookies
-  });
+// const getActiveProductsWithPrices = async (): Promise<ProductWithPrice[]> => {
+//   const supabase = createServerComponentClient({
+//     cookies: cookies
+//   });
 
-  const { data, error } = await supabase
-    .from('products')
-    .select('*, prices(*)')
-    .eq('active', true)
-    .eq('prices.active', true)
-    .order('metadata->index')
-    .order('unit_amount', { foreignTable: 'prices' });
+//   const { data, error } = await supabase
+//     .from('products')
+//     .select('*, prices(*)')
+//     .eq('active', true)
+//     .eq('prices.active', true)
+//     .order('metadata->index')
+//     .order('unit_amount', { foreignTable: 'prices' });
 
-  if (error) {
-    console.log(error.message);
-  }
+//   if (error) {
+//     console.log(error.message);
+//   }
 
-  return (data as any) || [];
-}
+//   return (data as any) || [];
+// }
 
-export default getActiveProductsWithPrices;
+// export default getActiveProductsWithPrices;
